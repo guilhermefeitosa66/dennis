897321bb23fb9730c0a9c128e29d67b3b3cb9012
Guilherme <guilhermefeitosa66@gmail.com>
2016-11-30 10:49:34 -0300

    fazendo busca dos arquivos xml para extração do tempo dos casos de teste

--- tempo.rb
+++ tempo.rb
@@ -1,15 +1,28 @@
 require 'spreadsheet'
 require 'nokogiri'
 require 'uri'
+require 'find'
 require 'axlsx' # gem install axlsx
 
 file = ""
 # ler arquivos xml
-Dir.glob("tempo/*/*.xml") do |xml_files|
+
+file_paths = []
+
+Find.find('tempo/') do |path|
+  file_paths << path if path =~ /.*\.xml$/
+end
+
+projects = []
+# Dir.glob("tempo/*/*.xml") do |xml_files|
+file_paths.each do |xml_files|
+  project_name = xml_files.split("/")[1]
+
   File.open("#{xml_files}", 'r') do |xml_file|
     while line = xml_file.gets
       if line.include? "testcase"
         file << line
+        # projects << project_name
       end
     end
   end
@@ -27,7 +40,7 @@ xml = Nokogiri::XML(file)
 Axlsx::Package.new do |p|
   p.workbook.add_worksheet(:name => "testcases") do |sheet|
     sheet.add_row %w{TESTCASE CLASS TIME}
-    xml.child.elements.each { |e| sheet.add_row([e.attributes["name"].value, e.attributes["classname"].value, e.attributes["time"].value]) }
+    xml.child.elements.each_with_index { |e,i| sheet.add_row([ e.attributes["name"].value, e.attributes["classname"].value, e.attributes["time"].value]) }
   end
   p.serialize('tempo.xlsx')
 end
