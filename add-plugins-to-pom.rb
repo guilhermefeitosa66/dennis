8d6a17f00adaf79e9ca3cd08bf467f0a007286c3
Guilherme <guilhermefeitosa66@gmail.com>
2016-12-30 12:33:37 -0300

    script para geração da cobertura e complexidade ciclomática

--- add-plugins-to-pom.rb
+++ add-plugins-to-pom.rb
@@ -4,10 +4,80 @@ require 'nokogiri'
 # ADD PLUGINS IN POM.XML
 pom_files = []
 
-Find.find("/home/guilherme/spring-projects/maven/") do |file|
-  if file.include? "pom.xml"
-    pom_files << file
-    puts file
+projects = [
+  # 'aws-maven',
+  # 'eclipse-integration-commons',
+  # 'eclipse-integration-gradle',
+  # 'eclipse-integration-tcserver',
+  # 'grails-ide',
+  # 'greenhouse',
+  'greenhouse-android',
+  'insight-plugin-mongodb',
+  # 'se-db4o',
+  # 'spring-amqp-samples',
+  # 'spring-analytics',
+  # 'spring-batch-admin',
+  # 'spring-boot',
+  # 'spring-bus',
+  # 'spring-data-aerospike',
+  # 'spring-data-book',
+  # 'spring-data-build',
+  # 'spring-data-cassandra',
+  # 'spring-data-commons',
+  # 'spring-data-couchbase',
+  # 'spring-data-dev-tools',
+  # 'spring-data-document-examples',
+  # 'spring-data-elasticsearch',
+  # 'spring-data-envers',
+  # 'spring-data-examples',
+  # 'spring-data-gemfire',
+  'spring-data-gemfire-sts-templates',
+  'spring-data-graph',
+  'spring-data-graph-examples',
+  # 'spring-data-jpa',
+  # 'spring-data-keyvalue',
+  # 'spring-data-ldap',
+  # 'spring-data-mongodb',
+  # 'spring-data-multistore-test',
+  # 'spring-data-neo4j',
+  # 'spring-data-redis',
+  # 'spring-data-rest',
+  # 'spring-data-solr',
+  # 'spring-flex',
+  # 'spring-flex-roo',
+  # 'spring-flo',
+  # 'spring-guice',
+  'spring-hadoop-samples',
+  # 'spring-hateoas',
+  # 'spring-ide',
+  # 'spring-insight-plugins',
+  # 'Spring-Integration-in-Action',
+  # 'spring-mvc-showcase',
+  # 'spring-petclinic',
+  # 'spring-plugin',
+  # 'spring-retry',
+  # 'spring-roo',
+  # 'spring-roo-community-addons',
+  # 'spring-roo-wrapping',
+  # 'spring-security-oauth',
+  'spring-security-portlet',
+  'spring-tenancy',
+  'spring-test-mvc'
+  # 'spring-tuple',
+  # 'spring-vault',
+  # 'spring-webflow-issues',
+  # 'spring-webflow-samples',
+  # 'spring-xd-samples',
+  # 'toolsuite-discovery-runtimes',
+  # 'toolsuite-distribution'
+  ]
+
+projects.each do |project|
+  Find.find("/home/guilherme/spring-projects/maven/#{project}") do |file|
+    if file.include? "pom.xml"
+      pom_files << file
+      puts file
+    end
   end
 end
 
@@ -45,7 +115,6 @@ pom_files.each do |path|
     <version>2.16</version>
     <configuration>
       <redirectTestOutputToFile>true</redirectTestOutputToFile>
-      <runOrder>random</runOrder>
     </configuration>
   </plugin>'
 
