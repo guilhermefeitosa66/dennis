3e01d90fa8c7b2f7cac53f265cb9e2f6a687f6ba
Guilherme <guilhermefeitosa66@gmail.com>
2017-05-11 18:10:56 -0300

    refazendo script para obtenção de cobertura por caso de teste

--- cobertura.rb
+++ cobertura.rb
@@ -6,85 +6,42 @@ require 'pp'
 require 'find'
 
 coverage = []
-file_paths = []
+classes_app = []
+classes_test = []
+projects = []
 
-# Find.find('cobertura/') do |path|
-Find.find('/home/guilherme/spring-projects/maven/') do |path|
-  file_paths << path if path =~ /.*\/testsrc-pkg-summary\.html$/
+# projects_path = '/home/guilherme/spring-projects/maven/'
+projects_path = 'maven/'
+
+Dir.glob('maven/*') do |dir|
+  projects << dir.split("/")[1]
+end
+
+## Localiza os arquivos que contem a lista de classes da app e de classes de teste,
+## para posteriormente obter os nomes de cada classes e os arquivos que contem seu relatório
+projects.each do |project|
+
+  Find.find("#{projects_path}/#{project}") do |path|
+    #classes_app << path if path.include?('site/clover/') && path.include?('pkg-summary.html')
+    #classes_test << path if path.include?('site/clover/') && path.include?('testsrc-pkg-summary.html')
+  end
 end
 
 # ler arquivos html
-file_paths.each do |html_files|
-  sumary = ""
-  file = ""
-  # ler o arquivo do sumário com o nome das classes
-  File.open("#{html_files}", 'r') do |s|
-    while line = s.gets
-      sumary << line
-    end
-  end
+classes_app.each do |html_files|
 
-  # extrai a lista de  classes do sumário
-  html = Nokogiri::HTML(sumary)
-
-  rows = html.xpath("//table[@id='packageClassesTable']/tbody/tr")
-  classes_list = rows.collect do |row|
-    class_name = {}
-    [
-      [:class_name, 'td[1]/span/text()'],
-    ].each do |name, xpath|
-      class_name[name] = row.at_xpath(xpath).to_s.strip
-    end
-    class_name
-  end
-
-  project_name = html_files.split("/")[5]
-  path = html_files.gsub("/testsrc-pkg-summary.html", "")
-  puts path
-
-  # extrair nome do método e cobertura
-  classes_list.each do |c|
-    c[:class_name] = c[:class_name].split(".")[0]
-
-    File.open("#{path}/#{c[:class_name]}.html", 'r') do |html_file|
-      while line = html_file.gets
-        file << line
-      end
-    end
-
-    html = Nokogiri::HTML(file)
-    id = "dialog-#{c[:class_name]}-methods-body"
-    rows = html.xpath("//table/tbody[@id='#{id}']/tr")
-
-    array = rows.collect do |row|
-      detail = {}
-      [
-        [:method_name, 'td[1]/span/text()'],
-        [:coverage, 'td[7]/span/text()'],
-      ].each do |name, xpath|
-        detail[name] = row.at_xpath(xpath).to_s.strip
-      end
-      detail[:method_name] = detail[:method_name].gsub("&lt;","<").gsub("&gt;",">")
-      detail[:coverage] = 0 if detail[:coverage].to_i < 0
-      detail
-    end
-
-    array.each do |e|
-      coverage << {project: project_name, class: c[:class_name], method_name: e[:method_name], coverage: e[:coverage]}
-    end
-  end
 end
 
-# gerar planilha com os dados
-Axlsx::Package.new do |p|
-  p.workbook.add_worksheet(:name => "testcases") do |sheet|
-    sheet.add_row %w{PROJECT CLASS METHOD COVERAGE}
-    coverage.each { |e| sheet.add_row([ e[:project], e[:class], e[:method_name], e[:coverage] ]) }
-  end
-  p.serialize('cobertura.xlsx')
-end
+# ## gerar planilha com os dados
+# Axlsx::Package.new do |p|
+#   p.workbook.add_worksheet(:name => "testcases") do |sheet|
+#     sheet.add_row %w{PROJECT CLASS METHOD COVERAGE}
+#     coverage.each { |e| sheet.add_row([ e[:project], e[:class], e[:method_name], e[:coverage] ]) }
+#   end
+#   p.serialize('cobertura.xlsx')
+# end
 
-puts "done!"
+# puts "done!"
 
 
 # <plugin>
