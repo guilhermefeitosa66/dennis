fa45a503c5f6cf1f3fc6e6f4bec30729ebf6a5f8
Guilherme <guilhermefeitosa66@gmail.com>
2017-05-12 11:42:35 -0300

    concluido script para extração da cobertura dos relatórios do clover

--- cobertura.rb
+++ cobertura.rb
@@ -5,45 +5,105 @@ require 'axlsx' # gem install axlsx
 require 'pp'
 require 'find'
 
-coverage = []
-classes_app = []
-classes_test = []
 projects = []
-row = 0
-col = 0
+classes_summary = []
+classes_app = []
+tests_case = []
+coverages = []
+
 
 # projects_path = '/home/guilherme/spring-projects/maven/'
-projects_path = 'maven/'
+projects_path = '/home/guilherme/dennis/maven/' # teste
 
+puts "lendo projetos..."
 Dir.glob('maven/*') do |dir|
   projects << dir.split("/")[1]
 end
 
-## Localiza os arquivos que contem a lista de classes da app e de classes de teste,
-## para posteriormente obter os nomes de cada classes e os arquivos que contem seu relatório
+## Localiza os arquivos que contem a lista de classes, para em seguida obter 
+## os nomes de cada arquivo que contem o relatório individual da classe
+puts "localizando sumário de classes..."
 projects.each do |project|
 
   Find.find("#{projects_path}/#{project}") do |path|
-    #classes_app << path if path.include?('site/clover/') && path.include?('pkg-summary.html')
+    classes_summary << path if path.include?('site/clover/') && path.include?('/pkg-summary.html')
     #classes_test << path if path.include?('site/clover/') && path.include?('testsrc-pkg-summary.html')
+  end
+
+  ## localiza a tabela 'packageClassesTable' em cada arquivo
+  puts "criando lista de classes do projeto: #{project}..."
+  classes_summary.each do |file|
+    html = Nokogiri::HTML(File.open(file))
+    rows = html.xpath("//table[@id='packageClassesTable']/tbody/tr")
+
+    ## percorre a tabela de classes da app localizada para adicionar o caminho do arquivo da classe e seu nome
+    rows.each do |tr|    
+      tr.at_xpath('td[1]/span/text()').to_s.strip.split('.').each do |single_class|
+        classes_app << [file.gsub('pkg-summary.html', ''), project, single_class]
+      end
+    end
+  end
+
+  ## percorre todos os arquivos de relatório da classe, para localizar os testes que a cobrem
+  puts "criando lista de casos de teste do projeto: #{project}..."
+  classes_app.each do |ca|
+    begin
+      html = Nokogiri::HTML(File.open("#{ca[0]}/#{ca[2]}.html".gsub('//','/') ))
+      rows = html.xpath("//tbody[@id='tests-body']/tr")
+
+      ## adiciona todos os casos de teste no vetor
+      rows.each do |tr|
+        if ! tests_case.include? tr.at_xpath('td[3]/span/text()').to_s.strip
+          tests_case << tr.at_xpath('td[3]/span/text()').to_s.strip
+        end
+      end
+    rescue Exception => e
+      next
+    end
+    
+  end
+
+end
+
+## percorre todos os arquivos de relatório da classe, para verificar quais testes a cobrem
+puts "construido matriz de cobertura por classes..."
+classes_app.each do |ca|
+  begin
+    html = Nokogiri::HTML(File.open("#{ca[0]}/#{ca[2]}.html".gsub('//','/') ))
+    rows = html.xpath("//tbody[@id='tests-body']/tr")
+
+    new_row = [ca[1], ca[2]]
+
+    ## adiciona as classes com a sua cobertura para cada caso de teste
+    tests_case.each do |tc|
+      coverage = 0
+
+      rows.each do |tr|
+        if tr.at_xpath('td[4]/span[2]/text()').to_s.strip == 'PASS' && tr.at_xpath('td[3]/span/text()').to_s.strip == tc
+          coverage = tr.at_xpath('td[2]/span/text()').to_s.strip
+        end
+      end
+
+      new_row << coverage
+    end
+
+    coverages << new_row
+  rescue Exception => e
+    next
   end
 end
 
-# ler arquivos html
-classes_app.each do |html_files|
-
+## gerar planilha com os dados
+puts "gerando planilha..."
+Axlsx::Package.new do |p|
+  p.workbook.add_worksheet(:name => "cobertura") do |sheet|
+    sheet.add_row (["PROJECT", "TESTCASE/CLASS"] + tests_case)
+    coverages.each { |row| sheet.add_row(row) }
+  end
+  p.serialize('cobertura.xlsx')
 end
 
-# ## gerar planilha com os dados
-# Axlsx::Package.new do |p|
-#   p.workbook.add_worksheet(:name => "testcases") do |sheet|
-#     sheet.add_row %w{PROJECT CLASS METHOD COVERAGE}
-#     coverage.each { |e| sheet.add_row([ e[:project], e[:class], e[:method_name], e[:coverage] ]) }
-#   end
-#   p.serialize('cobertura.xlsx')
-# end
-
-# puts "done!"
+puts "pronto!"
 
 
 # <plugin>
@@ -61,41 +121,3 @@ end
 
 # mvn clean clover:setup test clover:aggregate clover:clover ## executar primeiro
 # mvn clover:clover ## => gerar relatório em html
-
-## BEGIN TEST
-
-ts  = ["t1", "t2", "t3", "t4", "t5", "t6"]
-
-ps  = 
-[
-  ["projeto 1", "classe 1"],
-  ["projeto 1", "classe 2"],
-  ["projeto 1", "classe 3"],
-  ["projeto 2", "classe 1"],
-  ["projeto 2", "classe 2"],
-  ["projeto 2", "classe 3"]
-]
-
-p1 = 
-[
-  ["projeto 1", "classe 1", 1, 1, 1],
-  ["projeto 1", "classe 2", 1, 1, 1],
-  ["projeto 1", "classe 3", 1, 1, 1]
-]
-
-p2 = 
-[
-  ["projeto 2", "classe 1", 2, 2, 2],
-  ["projeto 2", "classe 2", 2, 2, 2],
-  ["projeto 2", "classe 3", 2, 2, 2]
-]
-
-r = 
-[
-  ["projeto 1", "classe 1", 1, 1, 1, 0, 0, 0],
-  ["projeto 1", "classe 2", 1, 1, 1, 0, 0, 0],
-  ["projeto 1", "classe 3", 1, 1, 1, 0, 0, 0],
-  ["projeto 2", "classe 1", 0, 0, 0, 2, 2, 2],
-  ["projeto 2", "classe 2", 0, 0, 0, 2, 2, 2],
-  ["projeto 2", "classe 3", 0, 0, 0, 2, 2, 2]
-]
